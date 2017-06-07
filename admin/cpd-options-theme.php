ffd2cdfb6ce28229809236ce20776ec6813f4e64
Matt Watson <matt@makedo.net>
2017-06-07 15:49:05 +0100

    Added editable global user guide link

--- admin/cpd-options-theme.php
+++ admin/cpd-options-theme.php
@@ -25,7 +25,7 @@ class CPD_Options_Theme {
 	 */
 	public static function get_instance() {
 		/**
-		 * If an instance hasn't been created and set to $instance create an instance 
+		 * If an instance hasn't been created and set to $instance create an instance
 		 * and set it to $instance.
 		 */
 		if ( null == self::$instance ) {
@@ -41,7 +41,7 @@ class CPD_Options_Theme {
 	 * @param      string    $version    The version of this plugin.
 	 */
 	public function __construct() {
-		
+
 	}
 
 	/**
@@ -49,40 +49,42 @@ class CPD_Options_Theme {
 	 *
 	 * @param      string    $text_domain       The text domain of the plugin.
 	 */
-	public function set_text_domain( $text_domain ) { 
+	public function set_text_domain( $text_domain ) {
 		$this->text_domain = $text_domain;
 	}
 
 	public function init_options_page() {
-		
+
 		/* Register Settings */
 		register_setting( 'cpd_settings_theme_group', 'cpd_theme_parent' );
 		register_setting( 'cpd_settings_theme_group', 'cpd_theme' );
+		register_setting( 'cpd_settings_theme_group', 'cpd_user_guide' );
 
 		/* Set defaults */
 		$cpd_theme_parent = get_option( 'cpd_theme_parent', NULL );
 		$cpd_theme = get_option( 'cpd_theme', NULL );
-		
+
 		if( $cpd_theme_parent == NULL ) {
-			
+
 			add_option( 'cpd_theme_parent', 'twentyfifteen' );
 
 			$cpd_theme_parent = get_option( 'cpd_theme_parent' );
 		}
 
 		if( $cpd_theme == NULL ) {
-			
+
 			add_option( 'cpd_theme', 'cpd-theme' );
 
 			$cpd_theme = get_option( 'cpd-theme' );
 		}
-		
+
 		/* Add sections */
 		add_settings_section( 'cpd_theme_defaults', 'Theme defaults', array( $this, 'cpd_theme_defaults_callback' ), 'cpd_settings_theme' );
-	
+
     	/* Add fields to a section */
 		add_settings_field( 'cpd_theme_parent', 'Parent Theme', array( $this, 'cpd_theme_parent_callback' ), 'cpd_settings_theme', 'cpd_theme_defaults' );
 		add_settings_field( 'cpd_theme', 'Theme', array( $this, 'cpd_theme_callback' ), 'cpd_settings_theme', 'cpd_theme_defaults' );
+		add_settings_field( 'cpd_user_guide', 'User Guide', array( $this, 'cpd_user_guide_callback' ), 'cpd_settings_theme', 'cpd_theme_defaults' );
 	}
 
 	/**
@@ -117,6 +119,16 @@ class CPD_Options_Theme {
 	}
 
 	/**
+	 * Render the field
+	 */
+	public function cpd_user_guide_callback() {
+		$cpd_user_guide = get_option( 'cpd_user_guide', 'https://github.com/mkdo/cpd/wiki' );
+		?>
+		<input type="text_url" name="cpd_user_guide" id="cpd_user_guide" value="<?php echo esc_url( $cpd_user_guide ); ?>"/>
+		<?php
+	}
+
+	/**
 	 * Add the options page
 	 */
 	public function add_options_page() {
@@ -126,16 +138,16 @@ class CPD_Options_Theme {
 	/**
 	 * Render the options page
 	 */
-	public function render_options_page(){ 
+	public function render_options_page(){
 		?>
-		<div class="wrap cpd-settings cpd-settings-theme">  
-			<h2>Theme Settings</h2> 
+		<div class="wrap cpd-settings cpd-settings-theme">
+			<h2>Theme Settings</h2>
 			<form action="/wp-admin/options.php" method="POST">
 	            <?php settings_fields( 'cpd_settings_theme_group' ); ?>
 	            <?php do_settings_sections( 'cpd_settings_theme' ); ?>
 	            <?php submit_button(); ?>
 	        </form>
-		</div> 
+		</div>
 	<?php
 	}
 }
--- templates/cpd-dashboard-widget-user-guide.php
+++ templates/cpd-dashboard-widget-user-guide.php
@@ -4,40 +4,38 @@
  */
 ?>
 <?php
+	$link                               = get_option( 'cpd_user_guide', 'https://github.com/mkdo/cpd/wiki' );
+	$text                               = '<p>You can view guidance on how to use this system by visiting the <a href="'. esc_url( $link ) .'" target="_blank">Wiki</a>.</p>';
+	$button_text                        = 'View Wiki';
 
+	$current_user 					    = wp_get_current_user();
+	$roles 							    = $current_user->roles;
+	$is_elevated_user 				    = get_user_meta( $current_user->ID, 'elevated_user', TRUE ) == '1';
 
-		$link                               = 'https://github.com/mkdo/cpd/wiki';
-		$text                               = '<p>You can view guidance on how to use this system by visiting the <a href="'. $link .'" target="_blank">Wiki</a>.</p>';
-		$button_text                        = 'View Wiki';
-
-		$current_user 					    = wp_get_current_user();
-		$roles 							    = $current_user->roles;
-		$is_elevated_user 				    = get_user_meta( $current_user->ID, 'elevated_user', TRUE ) == '1';
-
-		if( is_network_admin() || $is_elevated_user ) {
-			$link                               = 'https://github.com/mkdo/cpd/wiki/User-Guide';
-			$text                               = '<p>You can view guidance on how to setup and manage this system by viewing the <a href="'. $link .'" target="_blank">System Administrator User Guide</a>.</p>';
-			$button_text                        = 'View User Guide';
-		}
-		else if( in_array( 'supervisor', $roles ) ) {
-			$link                               = 'https://github.com/mkdo/cpd/wiki/User-Guide';
-			$text                               = '<p>You can view guidance on how to use this system by viewing the <a href="'. $link .'" target="_blank">Supervisor User Guide</a>.</p>';
-			$button_text                        = 'View User Guide';
-		}
-		else if( in_array( 'participant', $roles ) ) {
-			$link                               = 'https://github.com/mkdo/cpd/wiki/Participant-User-Guide';
-			$text                               = '<p>You can view guidance on how to manage your Journal by viewing the <a href="'. $link .'" target="_blank">Participant User Guide</a>.</p>';
-			$button_text                        = 'View User Guide';
-		}
+	if( is_network_admin() || $is_elevated_user ) {
+		$link                               = 'https://github.com/mkdo/cpd/wiki/User-Guide';
+		$text                               = '<p>You can view guidance on how to setup and manage this system by viewing the <a href="'. esc_url( $link ) .'" target="_blank">System Administrator User Guide</a>.</p>';
+		$button_text                        = 'View User Guide';
+	}
+	else if( in_array( 'supervisor', $roles ) ) {
+		$link                               = 'https://github.com/mkdo/cpd/wiki/User-Guide';
+		$text                               = '<p>You can view guidance on how to use this system by viewing the <a href="'. esc_url( $link ) .'" target="_blank">Supervisor User Guide</a>.</p>';
+		$button_text                        = 'View User Guide';
+	}
+	else if( in_array( 'participant', $roles ) ) {
+		$link                               = 'https://github.com/mkdo/cpd/wiki/Participant-User-Guide';
+		$text                               = '<p>You can view guidance on how to manage your Journal by viewing the <a href="'. esc_url( $link ) .'" target="_blank">Participant User Guide</a>.</p>';
+		$button_text                        = 'View User Guide';
+	}
 ?>
 <div>
 
 	<div class="content-description">
 
-		<?php echo $text;?>
+		<?php echo wp_kses_post( $text );?>
 
 	</div>
 
-	<p class="footer-button"><a class="button" href="<?php echo $link;?>"><?php echo $button_text;?></a></p>
+	<p class="footer-button"><a class="button" href="<?php echo esc_url( $link );?>"><?php echo $button_text;?></a></p>
 
 </div>
